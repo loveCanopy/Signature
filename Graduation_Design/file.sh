4b0191329ba25c7d3954ad7919172829a5addc85
root <root@DESKTOP-T7K5GSI.localdomain>
2017-11-05 16:04:08 +0800

    shell

--- Graduation_Design/file.sh
+++ Graduation_Design/file.sh
@@ -24,8 +24,10 @@ do
 			echo $hash_text
 			echo -e "\n"
 		done
+		#得到记录数
 		num=`awk -F "" '/比较次数/{print $5}' $dir/$i |awk '{num+=1 }END {print num}'`
-		number=`awk -F "" '/比较次数/{print $5}' $dir/$i|awk '{num+=$0 }END {print num}'`
+		#得到比较次数的总数
+		number=`awk -F ":" '/比较次数/{print $2}' $dir/$i|awk '{num+=$0 }END {print num}'`
 	#number=`awk '/SB|K|比较次数/{printf "%s\n",$0 }'| awk -F "" '/比较次数/{print $5}'|awk '{num+=$0 }END {print num}' $j`
 		echo $dir"---"$i,Hash比对数$number,记录数$num
 		hash_num=`awk 'BEGIN{printf "%0.5f",'$number'/'$num'}'`
